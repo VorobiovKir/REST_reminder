0c1b98135c328d05b1e18fe73730c8bcff2e1ef7
VorobiovKir <kvorobiov89@gmail.com>
2015-12-30 16:21:56 +0200

    'add angular route'

---
+++ site/js/routes.js
@@ -0,0 +1,19 @@
+angular
+
+    .module('MainApp')
+
+    .config(function($routeProvider) {
+        $routeProvider.when('/registration', {
+            templateUrl: '/templates/anonymous/registration.html'
+        })
+
+        .when('main/', {
+            templateUrl: '/templates/authorizated/main.html'
+        })
+
+        .when('/', {
+            templateUrl: '/templates/anonymous/login.html'
+        })
+
+        .otherwise( { redirectTo: '/' });
+    });
