4e8643ccd952093e2c3d692029bb3949e08f82a7
VorobiovKir <kvorobiov89@gmail.com>
2016-01-05 17:18:08 +0200

    add login and logout

--- site/js/controllers/AuthorController.js
+++ site/js/controllers/AuthorController.js
@@ -1,11 +1,11 @@
-var AuthorController = function($http, $window) {
+var AuthorController = function($http, $window, $scope) {
     var that = this;
 
-    this.isAuth = false;
+    this.isAuth = $window.sessionStorage.token || false;
     this.url = 'http://127.0.0.1:8000/';
 
     this.user = {
-        name: '',
+        name: $window.sessionStorage.username || '',
         email: '',
         password: '',
         r_password: '',
@@ -18,31 +18,40 @@ var AuthorController = function($http, $window) {
         };
 
         $http.post(that.url + 'rest-auth/login/', this.data)
-        .success(function(response) {
-            $window.sessionStorage.token = response.key;
-            console.log($window.sessionStorage);
-            that.isAuth = true;
-        })
-        .error(function(response) {
-            delete $window.sessionStorage.token;
-        });
+            .success(function(response) {
+                $window.sessionStorage.token = response.key;
+                $window.sessionStorage.username = that.user.name;
+                console.log($window.sessionStorage);
+                that.isAuth = true;
+            })
+            .error(function(response) {
+                delete $window.sessionStorage.token;
+            });
     };
 
     this.test = function(some) {
         $http
         .get('http://127.0.0.1:8000/rest-auth/user/')
             .success(function(response) {
-                console.log(response);
+                // console.log(response);
             })
             .error(function(response) {
                 alert('e');
             });
     };
 
+    this.test1 = function() {
+        // $window.sessionStorage.removeItem('token');
+        console.log($window.sessionStorage);
+    };
+
     this.logout = function() {
         $http.post('http://127.0.0.1:8000/rest-auth/logout/')
             .success(function(response) {
+                delete $window.sessionStorage.token;
+                delete $window.sessionStorage.username;
                 that.isAuth = false;
+                that.user = {};
             });
     }
 };
---
+++ site/js/filters/upperFirstLetter.js
@@ -0,0 +1,10 @@
+angular.module('MainApp')
+.filter('upperFirstLetter', function() {
+    return function(name) {
+        if (angular.isString(name)) {
+            return name.charAt(0).toUpperCase() + name.slice(1);
+        } else {
+            return name;
+        }
+    };
+});
